d2577fcea310cc5febb0b205aa516f83accce610
Borja Requena <brequena87@gmail.com>
2021-05-26 18:02:04 +0100

    Yelp MVP basically completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+noodles = { name: "I love Noodles", address: "Hoxton", category: "chinese" }
+pasta_muswell = { name: "I love Pasta", address: "Muswell Hill", category: "italian" }
+tapas = { name: "I love French Tapas, even if don't exist", address: "Hampstead", category: "french" }
+franco_manca = { name: "Franco Manca", address: "Greenwich", category: "italian" }
+fereshpolo = { name: "Feresh Polo from Belgium", address: "Oxford", category: "belgian" }
+
+[noodles, pasta_muswell, tapas, franco_manca, fereshpolo].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
